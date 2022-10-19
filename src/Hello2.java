bb867a36f65cfc18bc47cdaa5759651d9c72cae9
Prashanth yejje <pyejje@bitbucket.allmysons.com>
2022-10-19 12:48:28 +0530

    FrameProject pushing manisha

---
+++ src/Hello2.java
@@ -0,0 +1,26 @@
+
+public class Hello2 {
+
+	
+		public void m1() {
+		System.out.println("m1 is executed");
+		}
+		public void m2() {
+		System.out.println("m2 is executed");
+		}
+		public void m3() {
+		System.out.println("m3 is executed");
+		}
+		public static void main(String args[]) {
+		//classname objectnsme = new classname();
+		Hello2 m=new Hello2();
+		m.m1();
+		m.m2();
+		m.m3();
+		}
+		}
+		
+
+	
+
+
